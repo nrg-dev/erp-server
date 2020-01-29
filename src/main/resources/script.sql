e57f7cb0d192f8da0aad2e609fd9f1f5ee118f04
alexjeff <46997932+alexjeff@users.noreply.github.com>
2020-01-29 08:21:21 +0800

    Updated script

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -2,48 +2,56 @@ mongo localhost/admin;
 use ErpDB;
 db.createCollection("test")
 db.createCollection("randomNumber")
-
 db.randomNumber.insert({ "randomID" : 1,"poinvoicenumber" : 001,"poinvoicecode" : "INVPO","salesinvoicenumber" : 001,"salesinvoicecode":"INVSA"})
 db.randomNumber.insert({ "randomID" : 2,"vendorinvoicenumber" : 001,"vendorinvoicecode" : "VEN","customerinvoicenumber" : 001,"customerinvoicecode":"CUST"})
 db.randomNumber.insert({ "randomID" : 3,"categoryinvoicenumber" : 01,"categoryinvoicecode" : "CAT","productinvoicenumber" : 01,"productinvoicecode":"PROD"})
+db.randomNumber.insert({ "randomID" : 4,"itemnumber" : 001,"itemcode" : "ITEM"})
 
 
+
+
+show collections
+db.test.drop()
+db.dropDatabase()
+db.pOInvoiceDetails.remove( { } )
+db.pOInvoiceDetails.find();
+mongoexport --db ErpDB --collection test --out E:\home\test.json
+
+sudo mongoexport --db ErpDB --collection test --out /home/ec2-user/test.json
+
+-- Purchase
+db.pOInvoice.find();
+db.pOInvoice.remove( { } )
+db.pOInvoiceDetails.find();
+db.pOInvoiceDetails.remove( { } )
+-- Sales
+
+-- Customer
+db.customer.find();
+db.customer.remove( { } )
+db.vendor.drop()
+
+-- Venodr
+db.vendor.find();
+db.vendor.remove( { } )
+db.vendor.drop()
+
+-- category
+db.category.find();
+db.category.remove( { } )
+db.category.drop()
+-- Item or Product
+db.item.drop()
+db.item.find();
+db.item.remove( { } )
+
+-- Random Number
 db.randomNumber.drop()
-
 db.randomNumber.find();
 db.randomNumber.remove( { } )
 
 
-db.test.insert({"name" : "alex"})
-db.test.insert({"name" : "alex","address" : "annai nager","education" : "mca"})
-show collections
-db.test.drop()
-db.dropDatabase()
-db.purchaseOrder.remove( { } )
-db.purchaseOrder.find();
-mongoexport --db ErpDB --collection test --out E:\home\test.json
-
-sudo mongoexport --db ErpDB --collection test --out /home/ec2-user/test.json
-db.randomNumber.remove({"_id" :ObjectId("5e302b5419cf607a9c899bd3") });
-db.pOInvoice.update({"_id" :ObjectId("5e3028feb184020f4c20e256") },{$set : {"vendorname":"Alex Ubalton-VEN2"}});
-
-db.pOInvoiceDetails.update({"_id" :ObjectId("5e3028c3b184020f4c20e24d") },{$set : {"vendorname":"Alex Ubalton-VEN1"}});
-
-db.pOInvoiceDetails.update({"_id" :ObjectId("5e30051bb18402288c3c8dd8") },{$set : {"vendorname":"Alex Ubalton-VEN2"}});
 
 
 
-db.pOInvoice.find();
-db.pOInvoice.remove( { } )
-
-db.pOInvoiceDetails.find();
-db.pOInvoiceDetails.remove( { } )
-
-
-db.vendor.find();
-db.vendor.remove( { } )
-
-
-db.customer.find();
-db.customer.remove( { } )
 
