261532af2174e4448658fc839beebb8021761b3d
Nisho <nisholin07@gmail.com7@gmail.com>
2020-06-25 19:13:40 +0530

    Erp Angular Project service change

--- src/main/java/com/erp/service/EnquiryService.java
+++ src/main/java/com/erp/service/EnquiryService.java
@@ -1,6 +1,7 @@
 package com.erp.service;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.Filter;
@@ -9,13 +10,8 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
-import org.codehaus.jettison.json.JSONException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,21 +21,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 //import javax.enterprise.inject.Produces;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.erp.bo.ErpBo;
 import com.erp.dto.Enquiry;
 import com.erp.mongo.dal.RandomNumberDAL;
 import com.erp.util.Custom;
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 
 @SpringBootApplication
 @RestController
@@ -61,9 +54,8 @@ public class EnquiryService implements Filter {
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 
-		HttpServletRequest request = (HttpServletRequest) req;
-	    HttpServletResponse response = (HttpServletResponse) res;
-	    response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		HttpServletResponse response = (HttpServletResponse) res;
+		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "POST,PUT, GET, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
@@ -79,58 +71,25 @@ public class EnquiryService implements Filter {
 	public void destroy() {
 	}
 	
-	@PostMapping("/saveEnquiry")
-	@Produces(MediaType.APPLICATION_JSON)
-	public String saveEnquiry(@RequestBody String myData) throws JSONException {
-		  logger.info("----- Inside saveEnquiry Method Calling  -----");
-		  Gson gson = new Gson();
-		  Enquiry enquiry = new Enquiry();
-		  JsonParser jsonParser = new JsonParser(); 
-		  JsonElement element = jsonParser.parse(myData);
-		  String name = element.getAsJsonObject().get("name").getAsString();  
-		  String phonenumber = element.getAsJsonObject().get("phonenumber").getAsString();
-		  String emailId = element.getAsJsonObject().get("email").getAsString(); 
-		  String message = element.getAsJsonObject().get("msg").getAsString();
-		  String country = element.getAsJsonObject().get("country").getAsString(); 
-		  String enquirytype = element.getAsJsonObject().get("enquirytype").getAsString();
-		 
+	@CrossOrigin(origins = "http://localhost:8080")
+	@RequestMapping(value = "/saveEnquiry", method = RequestMethod.POST)
+	public ResponseEntity<?> saveEnquiry(@RequestBody Enquiry enquiry) {
+		logger.info("saveEnquiry");
 		try {
-			logger.info("------------ Inside Try Condition -------------");
-			enquiry.setName(name); 
-			enquiry.setPhonenumber(phonenumber);
-			enquiry.setEmail_ID(emailId); 
-			enquiry.setMessage(message);
-			enquiry.setCountry(country); 
-			enquiry.setEnquirytype(enquirytype);
-			enquiry.setAddeddate(Custom.getCurrentInvoiceDate());
-			enquiry.setStatus("Active"); 
-			logger.info("Name ----------->" + name);
-			logger.info("phonenumber ----------->" + phonenumber);
-			logger.info("Before calling Bo------------->");
+			enquiry.setStatus("Active");
+			enquiry.setAddeddate(Custom.getCurrentInvoiceDate()); 
+			logger.info("--- Before Calling Bo ----");
 			enquiry = bo.saveEnquiry(enquiry);
-			logger.info("Successfuly called Bo --------------->");
+			logger.info("--- After Calling Bo ----");
+			return new ResponseEntity<>(HttpStatus.OK); 
 		} catch (Exception e) {
-			e.printStackTrace();
+			logger.error("Exception-->" + e.getMessage());
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
 		}
-		return gson.toJson(enquiry);
+
+		finally {
+
+		}
 	}
 	
-	@GetMapping("/loadEnquiry")
-	@Produces(MediaType.APPLICATION_JSON)
-	public String loadEnquiry() throws JSONException {
-		List<Enquiry> enquirylist = null;
-		Gson gson = new Gson();
-		try {
-			logger.info("Before Calling load enquiry list");
-			enquirylist = bo.loadEnquiry(enquirylist);
-			logger.info("Successfully Called load enquiry list");
-		} catch (Exception e) {
-			logger.info("Exception ------------->" + e.getMessage());
-		} finally {
-
-		}
-		return gson.toJson(enquirylist);
-	}
-
-
 }
